cbf49878f2555105517af93a87e87c6056f8c647
Nathan Cardoso <113688841+natejcq@users.noreply.github.com>
2023-09-01 20:54:02 +0530

    Register page details successfully connected to Database
    Now details of the user are stored in MongoDB upon registration

--- api/index.js
+++ api/index.js
@@ -5,13 +5,20 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json()); 
 const mongoose = require('mongoose');
+const User = require('./models/User');
 
 mongoose.connect('mongodb+srv://nathancardoso:Stnm112m6FDO9nzr@cluster0.oihvcna.mongodb.net/?retryWrites=true&w=majority')
 
-app.post('/register', (req,res) => {
+app.post('/register', async (req,res) => {
     const {username, password} = req.body;
-    console.log("WORKS");
-    res.json({requestData:{username, password}});
+    try {
+        const userDoc = await User.create({username: username, password: password});
+        res.json(userDoc);
+    } catch (error) {
+        console.error('Registration failed:', error);
+        res.status(400).json({ error: 'Registration failed' });
+
+    }
 
 });
 
--- api/models/User.js
+++ api/models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const {Schema, model} = mongoose;
 
 const UserSchema = new mongoose.Schema({
     username:{type: 'string', required: true, min:2, unique: true},
-    password:{type:'string', required: true, min:6},
+    password:{type:'string', required: true, min:6},    
 });
 
 const UserModel = model('User', UserSchema);
